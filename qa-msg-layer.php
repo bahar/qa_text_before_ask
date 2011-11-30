4d22c14ac5aadecf4d0eecf24d97925d596e0631
NoahY <noahy57@gmail.com>
2011-11-30 21:26:07 +0530

    fixes

--- qa-msg-layer.php
+++ qa-msg-layer.php
@@ -6,23 +6,24 @@ class qa_html_theme_layer extends qa_html_theme_base {
 
     function head_script() { // add a Javascript file from plugin directory
         qa_html_theme_base::head_script();
-        $proceed = '<input type="checkbox" name="advised" id="advised" value="advised"><label for="advised" id="adviseLabel">&nbsp; thanks, I will keep these tips in mind when asking</label><div style="text-align:right; width:100%; color:#888"><a href="#" onclick="showAskForm();"><b>proceed &raquo;</b></a></div>';
 
-        $final = qa_opt('msg_before_ask'); //.$proceed;
+        $final = str_replace("\n",'\\n',qa_opt('msg_before_ask')); 
+        $proceed = '<input type="checkbox" name="advised" id="advised" value="advised"><label for="advised" id="adviseLabel">&nbsp; '.qa_opt('msg_before_ask_label').'</label><div style="text-align:right; width:100%; color:#888"><a href="#" onclick="showAskForm();"><b>'.qa_opt('msg_before_ask_proceed').' &raquo;</b></a></div>';
+
         $custom = " var divClone = '';
             $(document).ready(function() {
                  divClone = $('div.qa-main').clone();
                  $('div.qa-main').html('$final');
                  $('div.qa-main').append('$proceed');
-                 })";
+             })";
 
         $proceedMsg = "
             function showAskForm() {
                  if($('#advised').is(':checked')) $('div.qa-main').replaceWith(divClone);
                  else($('#adviseLabel').attr('style','color:red; font-weight:bold;'))
-                 }";
+             }";
 
-        if ($this->template == 'ask' && !qa_opt('disable_msg_before_ask')) {
+        if ($this->template == 'ask' && qa_opt('enable_msg_before_ask')) {
             $this->output('<SCRIPT TYPE="text/javascript">', $custom, '</SCRIPT>');
             $this->output('<SCRIPT TYPE="text/javascript">', $proceedMsg, '</SCRIPT>');
         }
@@ -30,7 +31,7 @@ class qa_html_theme_layer extends qa_html_theme_base {
 
     function head_css() {
         qa_html_theme_base::head_css();
-        if ($this->template == 'ask' && !qa_opt('disable_msg_before_ask')) {
+        if ($this->template == 'ask' && qa_opt('enable_msg_before_ask')) {
             $this->output('<style>', qa_opt('msg_before_ask_css'), '</style>');
         }
     }
