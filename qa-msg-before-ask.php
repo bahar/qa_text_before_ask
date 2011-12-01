7bc3c7cdf95b89cebda981f34e4631de5d26471c
NoahY <noahy57@gmail.com>
2011-12-01 14:50:19 +0530

    fixed problems with other javascript, added default message

--- qa-msg-before-ask.php
+++ qa-msg-before-ask.php
@@ -14,6 +14,25 @@ class qa_msg_before_ask {
 	    switch($option) {
             case 'msg_before_ask_label':
                 return 'thanks, I will keep these tips in mind when asking';
+            case 'msg_before_ask':
+                return '<h1>Welcome to our support desk</h1>
+<p>We&rsquo;d love to help you. But not every question on our Support Desk gets answered. To <strong>improve your chances</strong>, here are some tips:</p>
+<h2>Do your homework</h2>
+<br />
+<p>Have you <b>thoroughly searched for an answer</b> <i>before</i> asking your question? Sharing your research helps everyone. Tell us what you found and why it didn&rsquo;t meet your needs. This demonstrates that you&rsquo;ve taken the time to try to help yourself, it saves us from reiterating obvious answers, and most of all it helps <i>you</i> get a more specific and relevant answer!</p>
+<h2>Be specific</h2>
+<br />
+<p>If you ask a vague question, you&rsquo;ll get a vague answer. But if you give us details and context, we can provide a <i>useful</i> answer.</p>
+<h2>Make it relevant to others</h2>
+<p>We like to help as many people at a time as we can. Make it clear how your question is relevant to more people than just you, and more of us will be interested in your question and willing to look into it.</p>
+<h2>Be on-topic</h2>
+<br />
+<p>Our support desk is defined by a specific set of topics in <a href="faq">the faq</a>; please try to stick to those topics. If your question is about the site itself, provide us <a href="http://thesixdegree.com/support/feedback">feedback</a>.</p>
+<h2>Keep an open mind</h2>
+<br />
+<p>The answer to your question may not always be the one you wanted, but that doesn&rsquo;t mean it is wrong. A conclusive answer isn&rsquo;t always possible. When in doubt, ask people to cite their sources, or to explain how/where they learned something. Even if we don&rsquo;t agree with you, or tell you exactly what you wanted to hear, remember &ndash; we&rsquo;re just trying to help.</p>
+<br />
+<br />';
             case 'msg_before_ask_proceed':
                 return 'proceed';
             default:
